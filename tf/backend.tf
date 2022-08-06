69c6fbdb1cd30b80639c723688d471212e219676
nugangha <52476525+nugangha@users.noreply.github.com>
2022-08-05 23:50:57 -0400

    Create backend.tf

---
+++ tf/backend.tf
@@ -0,0 +1,28 @@
+resource "vault_audit" "audit_dev" {
+  provider = vault.vault_dev
+  type     = "file"
+
+  options = {
+    file_path = "/vault/logs/audit"
+  }
+}
+
+resource "vault_audit" "audit_prod" {
+  provider = vault.vault_prod
+  type     = "file"
+
+  options = {
+    file_path = "/vault/logs/audit"
+  }
+}
+
+resource "vault_auth_backend" "userpass_dev" {
+  provider = vault.vault_dev
+  type     = "userpass"
+}
+
+
+resource "vault_auth_backend" "userpass_prod" {
+  provider = vault.vault_prod
+  type     = "userpass"
+}
