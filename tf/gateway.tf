1d1d4a5c82e3127a93fbf5f6dcf01acb7efca227
nugangha <52476525+nugangha@users.noreply.github.com>
2022-08-08 17:58:20 -0400

    Update gateway.tf

--- tf/gateway.tf
+++ tf/gateway.tf
@@ -35,3 +35,40 @@ resource "vault_generic_endpoint" "gateway_development" {
 EOT
 }
 
+#---------------------
+resource "vault_generic_secret" "gateway_production" {
+  provider = vault.vault_prod
+  path     = "secret/production/gateway"
+
+  data_json = <<EOT
+{
+  "db_user":   "gateway",
+  "db_password": "33fc0cc8-b0e3-4c06-8cf6-c7dce2705329"
+}
+EOT
+}
+
+resource "vault_policy" "gateway_production" {
+  provider = vault.vault_prod
+  name     = "gateway-production"
+
+  policy = <<EOT
+path "secret/data/production/gateway" {
+    capabilities = ["list", "read"]
+}
+EOT
+}
+
+resource "vault_generic_endpoint" "gateway_production" {
+  provider             = vault.vault_prod
+  depends_on           = [vault_auth_backend.userpass_prod]
+  path                 = "auth/userpass/users/gateway-production"
+  ignore_absent_fields = true
+
+  data_json = <<EOT
+{
+  "policies": ["gateway-production"],
+  "password": "123-gateway-production"
+}
+EOT
+}
