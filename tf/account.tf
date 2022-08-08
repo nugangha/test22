7d272ee6849316c0d162d4056e759e6e210e0530
nugangha <52476525+nugangha@users.noreply.github.com>
2022-08-07 23:01:10 -0400

    Create account.tf

---
+++ tf/account.tf
@@ -0,0 +1,36 @@
+resource "vault_generic_secret" "account_development" {
+  provider = vault.vault_dev
+  path     = "secret/development/account"
+
+  data_json = <<EOT
+{
+  "db_user":   "account",
+  "db_password": "965d3c27-9e20-4d41-91c9-61e6631870e7"
+}
+EOT
+}
+
+resource "vault_policy" "account_development" {
+  provider = vault.vault_dev
+  name     = "account-development"
+
+  policy = <<EOT
+path "secret/data/development/account" {
+    capabilities = ["list", "read"]
+}
+EOT
+}
+
+resource "vault_generic_endpoint" "account_development" {
+  provider             = vault.vault_dev
+  depends_on           = [vault_auth_backend.userpass_dev]
+  path                 = "auth/userpass/users/account-development"
+  ignore_absent_fields = true
+
+  data_json = <<EOT
+{
+  "policies": ["account-development"],
+  "password": "123-account-development"
+}
+EOT
+}
