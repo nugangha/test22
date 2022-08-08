06eb898bf0b77c8405829da2b6ee0272e1ee3066
nugangha <52476525+nugangha@users.noreply.github.com>
2022-08-08 18:16:27 -0400

    Update gateway.tf

--- tf/gateway.tf
+++ tf/gateway.tf
@@ -72,3 +72,45 @@ resource "vault_generic_endpoint" "gateway_production" {
 }
 EOT
 }
+
+#----------------------------
+resource "docker_container" "gateway_development" {
+  image = "form3tech-oss/platformtest-gateway"
+  name  = "gateway_development"
+
+  env = [
+    "VAULT_ADDR=http://vault-development:8200",
+    "VAULT_USERNAME=gateway-development",
+    "VAULT_PASSWORD=123-gateway-development",
+    "ENVIRONMENT=development"
+  ]
+
+  networks_advanced {
+    name = "vagrant_development"
+  }
+
+  lifecycle {
+    ignore_changes = all
+  }
+}
+
+resource "docker_container" "gateway_production" {
+  image = "form3tech-oss/platformtest-gateway"
+  name  = "gateway_production"
+
+  env = [
+    "VAULT_ADDR=http://vault-production:8200",
+    "VAULT_USERNAME=gateway-production",
+    "VAULT_PASSWORD=123-gateway-production",
+    "ENVIRONMENT=production"
+  ]
+
+  networks_advanced {
+    name = "vagrant_production"
+  }
+
+  lifecycle {
+    ignore_changes = all
+  }
+}
+
