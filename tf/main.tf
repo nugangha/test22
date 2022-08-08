0529b6f321e95d4ff676bf3f32c6daaca253b25b
nugangha <52476525+nugangha@users.noreply.github.com>
2022-08-07 22:54:20 -0400

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -1,20 +1,3 @@
-
-# user auth
-resource "vault_auth_backend" "userpass_dev" {
-  provider = vault.vault_dev
-  type     = "userpass"
-}
-
-resource "vault_auth_backend" "userpass_staging" {
-  provider = vault.vault_staging
-  type     = "userpass"
-}
-
-resource "vault_auth_backend" "userpass_prod" {
-  provider = vault.vault_prod
-  type     = "userpass"
-}
-
 #account
 
 resource "vault_generic_secret" "account_development" {
