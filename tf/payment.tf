64ff0e1b37bf8aee8aef6570305d7249b650c3e2
nugangha <52476525+nugangha@users.noreply.github.com>
2022-08-08 18:02:55 -0400

    Update payment.tf

--- tf/payment.tf
+++ tf/payment.tf
@@ -35,3 +35,44 @@ resource "vault_generic_endpoint" "payment_development" {
 EOT
 }
 
+#-------------------------
+resource "vault_generic_secret" "payment_production" {
+  provider = vault.vault_prod
+  path     = "secret/production/payment"
+
+  data_json = <<EOT
+{
+  "db_user":   "payment",
+  "db_password": "821462d7-47fb-402c-a22a-a58867602e39"
+}
+EOT
+}
+
+resource "vault_policy" "payment_production" {
+  provider = vault.vault_prod
+  name     = "payment-production"
+
+  policy = <<EOT
+path "secret/data/production/payment" {
+    capabilities = ["list", "read"]
+}
+EOT
+}
+
+resource "vault_generic_endpoint" "payment_production" {
+  provider             = vault.vault_prod
+  depends_on           = [vault_auth_backend.userpass_prod]
+  path                 = "auth/userpass/users/payment-production"
+  ignore_absent_fields = true
+
+  data_json = <<EOT
+{
+  "policies": ["payment-production"],
+  "password": "123-payment-production"
+}
+EOT
+}
+
+
+
+
