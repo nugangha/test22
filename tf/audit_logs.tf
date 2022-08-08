50e7fac2955598437aa6bf70a90cd69e4ee20190
nugangha <52476525+nugangha@users.noreply.github.com>
2022-08-07 22:51:18 -0400

    Create audit_logs.tf

---
+++ tf/audit_logs.tf
@@ -0,0 +1,26 @@
+resource "vault_audit" "audit_dev" {
+  provider = vault.vault_dev
+  type     = "file"
+
+  options = {
+    file_path = "/vault/logs/audit"
+  }
+}
+
+resource "vault_audit" "audit_staging" {
+  provider = vault.vault_staging
+  type     = "file"
+
+  options = {
+    file_path = "/vault/logs/audit"
+  }
+}
+
+resource "vault_audit" "audit_prod" {
+  provider = vault.vault_prod
+  type     = "file"
+
+  options = {
+    file_path = "/vault/logs/audit"
+  }
+}
