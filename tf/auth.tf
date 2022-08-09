e96148ca2cea36bfc241857b01f9573e2899e12a
nugangha <52476525+nugangha@users.noreply.github.com>
2022-08-08 20:07:13 -0400

    Update auth.tf

--- tf/auth.tf
+++ tf/auth.tf
@@ -3,12 +3,12 @@ resource "vault_auth_backend" "userpass_dev" {
   provider = vault.vault_dev
   type     = "userpass"
 }
-
+/*
 resource "vault_auth_backend" "userpass_staging" {
   provider = vault.vault_staging
   type     = "userpass"
 }
-
+*/
 resource "vault_auth_backend" "userpass_prod" {
   provider = vault.vault_prod
   type     = "userpass"
